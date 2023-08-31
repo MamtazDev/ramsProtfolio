2dd613617dc84380483fb40f1aa8ac6d4bfdfbc9
mohi14 <mohiuddinkhan1409@gmail.com>
2023-08-31 14:54:05 +0600

    some changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/routes";
