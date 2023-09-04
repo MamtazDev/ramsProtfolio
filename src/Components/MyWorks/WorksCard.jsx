4f9bb95263c51d27fdb1cb5dc92a633d64ba6658
mohi14 <mohiuddinkhan1409@gmail.com>
2023-09-04 18:49:04 +0600

    all issues solved

--- src/Components/MyWorks/WorksCard.jsx
+++ src/Components/MyWorks/WorksCard.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import rightArrow from "../../assets/ArrowRight.png";
+import { Link } from "react-router-dom";
 
 const WorksCard = ({ content }) => {
   return (
-    <div className="workCardContainer">
-      <div className="cardImageContainer">
-        <img src={content.image} alt="" className="w-100 h-100" />
-      </div>
-      <div className="cardTextContainer">
-        <h6>
-          <a href="#">{content.title}</a>
-        </h6>
-        <p>{content.skill}</p>
-        <div>
-          <a href="#">View Case Study</a>
-          <img src={rightArrow} alt="" />
+    <Link to="/project">
+      <div className="workCardContainer">
+        <div className="cardImageContainer">
+          <img src={content.image} alt="" className="w-100 h-100" />
+        </div>
+        <div className="cardTextContainer">
+          <h6>
+            <a href="#">{content.title}</a>
+          </h6>
+          <p>{content.skill}</p>
+          <div>
+            <a href="#">View Case Study</a>
+            <img src={rightArrow} alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
